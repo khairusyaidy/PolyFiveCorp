a78f19b041ec4a268037fa51f86faff12328c840
Khai <Khai@Khai-PC>
2013-01-28 15:15:40 +0800

    checkDate.java

---
+++ PolyFiveCorp/src/polyfive/entities/CheckDate.java
@@ -0,0 +1,14 @@
+package polyfive.entities;
+
+public class CheckDate {
+private  boolean checkDate;
+
+public boolean isCheckDate() {
+	return checkDate;
+}
+
+public void setCheckDate(boolean checkDate) {
+	this.checkDate = checkDate;
+}
+
+}
